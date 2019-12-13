6b276cc4ee9c1530bd35ad7d92a0d071d0917098
maerd19 <revolutions_333@hotmail.com>
2019-12-12 21:00:06 -0600

    one more change

--- api-suggox/app.js
+++ api-suggox/app.js
@@ -41,9 +41,9 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use("/api", surveyRoutes);
 
-// app.use('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 const port = process.env.PORT || 3000;
 
